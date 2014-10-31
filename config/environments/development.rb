8be530899e8f7ff17604dd46d824443b715262b0
ducan <ducan2408@gmail.com>
2014-10-31 10:59:44 +0700

    add git Ignore file and base generate config of Home page, user, post,comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
 
   config.action_mailer.delivery_method = :smtp
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031020603) do
+ActiveRecord::Schema.define(version: 20141031030804) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "users_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.integer  "users_id"
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
