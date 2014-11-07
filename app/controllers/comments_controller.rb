c550bae34ed7fc36d53aca615278734e1951a8cc
ducan <ducan2408@gmail.com>
2014-11-07 14:22:04 +0700

    fix layout home, and translation, ajax c

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,31 @@
 class CommentsController < ApplicationController
-	
+	before_action :authenticate_user!
+  respond_to :html, :xml, :json
+
+  # def self.when_to_run
+  #   2.hours.from_now
+  # end
+
+  # handle_asynchronously :call_a_class_method, :run_at => Proc.new { when_to_run }
 	def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    @post = Post.friendly.find(params[:post_id])
+    @comment = @post.comment.create(comment_params)
+    @comment.user_id = current_user.id #or whatever is you session name
+    if @comment.save
+        respond_to do |format|
+         format.json { render json: @comment.as_json(include: [:user])}
+        end
+        if @post.user!=current_user 
+          PostMail.delay.notication_email(@comment)
+      end
+      else
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+        flash.now[:danger] = "error"
+      end
   end
  
   private
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:content,:user_id, :post_id)
     end
 end
